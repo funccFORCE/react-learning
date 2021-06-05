a2161bc3f71f16b9da25b7e80091befc6177f4ab
AyshaHakeem <ayshapp468@gmail.com>
2021-06-05 11:55:14 +0530

    changed code of TodoList from functional component to class based component

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,14 +2,24 @@ import React from 'react';
 import todoInfo from './todoInfo'
 import TodoItem from './TodoItem';
 
-function TodoList () {
-  const todo= todoInfo.map(
-    item => <TodoItem id={item.id} text={item.text} check= {item.completed && 'true'} /> )
+class TodoList extends React.Component {
+
+  constructor(){
+    super();
+    this.state={
+      todos: todoInfo
+    }
+  }
+  render(){
+  const todo= this.state.todos.map(
+    item => <TodoItem id={item.id} text={item.text} check= {item.completed && 'true'} /> 
+    )
   return (
 <div className='todo-list'> 
   <h3 className='head'>To-Do List </h3>
   {todo}
 </div>
   )
+  }
 }
 export default TodoList;
